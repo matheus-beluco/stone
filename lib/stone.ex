b3e5f541db42d7c145a8e4c29bd99d8fb6db66a4
Matheus Beluco <math.beluco@gmail.com>
2022-08-16 17:45:05 -0300

    adding implementation to validate if params is an empty list

--- lib/stone.ex
+++ lib/stone.ex
@@ -1,16 +1,22 @@
 defmodule Stone do
   @moduledoc false
 
-  @spec split_the_bill(list(map()), list()) :: map()
+  @spec split_the_bill(list(map()), list()) :: {:ok, map()} | {:error, binary()}
+  def split_the_bill(shopping_list, emails)
+
+  def split_the_bill(shopping_list, emails)
+      when length(shopping_list) == 0 or length(emails) == 0 do
+    {:error, "invalid params"}
+  end
+
   def split_the_bill(shopping_list, emails) do
     bill = calculates_bill(shopping_list)
     payers = Enum.count(emails)
     value_per_payer = div(bill, payers) * 100
     values = Enum.map(1..payers, fn _ -> value_per_payer end)
+    split_bill = emails |> Enum.zip(values) |> Map.new()
 
-    emails
-    |> Enum.zip(values)
-    |> Map.new()
+    {:ok, split_bill}
   end
 
   defp calculates_bill(shopping_list) do
