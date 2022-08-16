a22fbf18d2dae85ec2a9685a13b8b008a4ec89d6
Matheus Beluco <math.beluco@gmail.com>
2022-08-16 16:09:03 -0300

    adding test that returns the amount that each email must pay

--- lib/stone.ex
+++ lib/stone.ex
@@ -1,2 +1,10 @@
 defmodule Stone do
+  def split_the_bill(_shopping_list, _emails) do
+    %{
+      "john@email.com": 65.500,
+      "lee@email.com": 65.500,
+      "helena@email.com": 65.500,
+      "marcos@email.com": 65.500
+    }
+  end
 end
--- test/stone_test.exs
+++ test/stone_test.exs
@@ -1,3 +1,24 @@
 defmodule StoneTest do
   use ExUnit.Case
+
+  describe "split_the_bill/2" do
+    test "returns the amount that each email must pay" do
+      shopping_list = [
+        %{item: "celular", amount: 10, unit_price: 200},
+        %{item: "tv", amount: 5, unit_price: 100},
+        %{item: "cadeira", amount: 4, unit_price: 30}
+      ]
+
+      emails = ["john@email.com", "lee@email.com", "helena@email.com", "marcos@email.com"]
+
+      expected_response = %{
+        "john@email.com": 65.500,
+        "lee@email.com": 65.500,
+        "helena@email.com": 65.500,
+        "marcos@email.com": 65.500
+      }
+
+      assert Stone.split_the_bill(shopping_list, emails) == expected_response
+    end
+  end
 end
