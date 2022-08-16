37b80503608bfb030d215f6a176db75e1e1914a2
Matheus Beluco <math.beluco@gmail.com>
2022-08-16 17:39:51 -0300

    adding tests to validate if params is an empty list

--- test/stone_test.exs
+++ test/stone_test.exs
@@ -18,7 +18,19 @@ defmodule StoneTest do
         "marcos@email.com" => 65500
       }
 
-      assert Stone.split_the_bill(shopping_list, emails) == expected_response
+      assert Stone.split_the_bill(shopping_list, emails) == {:ok, expected_response}
+    end
+
+    test "returns error if shopping list is empty" do
+      shopping_list = []
+      emails = ["john@email.com", "lee@email.com", "helena@email.com", "marcos@email.com"]
+      assert Stone.split_the_bill(shopping_list, emails) == {:error, "invalid params"}
+    end
+
+    test "returns error if emails is empty" do
+      shopping_list = [%{item: "celular", amount: 10, unit_price: 200}]
+      emails = []
+      assert Stone.split_the_bill(shopping_list, emails) == {:error, "invalid params"}
     end
   end
 end
