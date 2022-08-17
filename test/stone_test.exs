558ce02c8dfdf9164476439819b1997783505c5a
Matheus Beluco <math.beluco@gmail.com>
2022-08-17 16:35:20 -0300

    adding test to validate when bill is zero

--- test/stone_test.exs
+++ test/stone_test.exs
@@ -113,5 +113,12 @@ defmodule StoneTest do
 
       assert Stone.split_the_bill(shopping_list, emails) == {:error, "invalid params"}
     end
+
+    test "returns error if bill is zero" do
+      shopping_list = [%{item: "celular", amount: 10, unit_price: 0}]
+      emails = ["john@email.com", "lee@email.com"]
+
+      assert Stone.split_the_bill(shopping_list, emails) == {:error, "invalid params"}
+    end
   end
 end
