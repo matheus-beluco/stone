08c90c37fa4f52598e2c9e5a45032d4f54f5eb15
Matheus Beluco <math.beluco@gmail.com>
2022-08-17 12:58:35 -0300

    adding implementation to validate negative values in the shopping list

--- lib/stone.ex
+++ lib/stone.ex
@@ -10,18 +10,27 @@ defmodule Stone do
   end
 
   def split_the_bill(shopping_list, emails) do
-    bill = calculates_bill(shopping_list)
-    payers = Enum.count(emails)
-    value_per_payer = div(bill, payers) * 100
-    values = Enum.map(1..payers, fn _ -> value_per_payer end)
-    split_bill = emails |> Enum.zip(values) |> Map.new()
+    with {:ok, shopping_list} <- validate_shopping_list(shopping_list) do
+      bill = calculates_bill(shopping_list)
+      payers = Enum.count(emails)
+      value_per_payer = div(bill, payers) * 100
+      values = Enum.map(1..payers, fn _ -> value_per_payer end)
+      split_bill = emails |> Enum.zip(values) |> Map.new()
 
-    {:ok, split_bill}
+      {:ok, split_bill}
+    end
   end
 
   defp calculates_bill(shopping_list) do
     Enum.reduce(shopping_list, 0, fn item, acc ->
       item[:amount] * item[:unit_price] + acc
     end)
+  end
+
+  defp validate_shopping_list(shopping_list) do
+    case Enum.any?(shopping_list, fn item -> item[:amount] < 0 || item[:unit_price] < 0 end) do
+      true -> {:error, "invalid params"}
+      false -> {:ok, shopping_list}
+    end
   end
 end
