d6afcb66f8c0b45833d85c53973bd6418462e4f8
Matheus Beluco <math.beluco@gmail.com>
2022-08-17 14:59:34 -0300

    adding tests to validate the value that each email must pay when the division is not exact

--- test/stone_test.exs
+++ test/stone_test.exs
@@ -2,7 +2,7 @@ defmodule StoneTest do
   use ExUnit.Case
 
   describe "split_the_bill/2" do
-    test "returns the value that each email must pay" do
+    test "returns the value that each email must pay when the division is exact" do
       shopping_list = [
         %{item: "celular", amount: 10, unit_price: 200},
         %{item: "tv", amount: 5, unit_price: 100},
@@ -16,6 +16,23 @@ defmodule StoneTest do
         "lee@email.com" => 65500,
         "helena@email.com" => 65500,
         "marcos@email.com" => 65500
+      }
+
+      assert Stone.split_the_bill(shopping_list, emails) == {:ok, expected_response}
+    end
+
+    test "returns the value that each email must pay when the division is not exact" do
+      shopping_list = [
+        %{item: "celular", amount: 1, unit_price: 50},
+        %{item: "tv", amount: 2, unit_price: 25}
+      ]
+
+      emails = ["john@email.com", "lee@email.com", "helena@email.com"]
+
+      expected_response = %{
+        "john@email.com" => 3300,
+        "lee@email.com" => 3300,
+        "helena@email.com" => 3400,
       }
 
       assert Stone.split_the_bill(shopping_list, emails) == {:ok, expected_response}
