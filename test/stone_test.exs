ef657be05fafb3f75c079f570e286e3efb81421a
Matheus Beluco <math.beluco@gmail.com>
2022-08-17 12:53:48 -0300

    adding tests to validate negative values in the shopping list

--- test/stone_test.exs
+++ test/stone_test.exs
@@ -2,7 +2,7 @@ defmodule StoneTest do
   use ExUnit.Case
 
   describe "split_the_bill/2" do
-    test "returns the amount that each email must pay" do
+    test "returns the value that each email must pay" do
       shopping_list = [
         %{item: "celular", amount: 10, unit_price: 200},
         %{item: "tv", amount: 5, unit_price: 100},
@@ -24,12 +24,28 @@ defmodule StoneTest do
     test "returns error if shopping list is empty" do
       shopping_list = []
       emails = ["john@email.com", "lee@email.com", "helena@email.com", "marcos@email.com"]
+
       assert Stone.split_the_bill(shopping_list, emails) == {:error, "invalid params"}
     end
 
     test "returns error if emails is empty" do
       shopping_list = [%{item: "celular", amount: 10, unit_price: 200}]
       emails = []
+
+      assert Stone.split_the_bill(shopping_list, emails) == {:error, "invalid params"}
+    end
+
+    test "returns error if unit_price is negative" do
+      shopping_list = [%{item: "celular", amount: 10, unit_price: -1}]
+      emails = ["john@email.com"]
+
+      assert Stone.split_the_bill(shopping_list, emails) == {:error, "invalid params"}
+    end
+
+    test "returns error if amount is negative" do
+      shopping_list = [%{item: "celular", amount: -1, unit_price: 10}]
+      emails = ["john@email.com"]
+
       assert Stone.split_the_bill(shopping_list, emails) == {:error, "invalid params"}
     end
   end
